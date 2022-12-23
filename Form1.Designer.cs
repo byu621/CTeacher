cd14b78193f30112209d2da75011be1188423c6a
Benjamin Yu <byu621@aucklanduni.ac.nz>
2022-12-23 17:01:31 +1300

    space bar is working now

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,51 @@ namespace CTeacher
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.character1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reveal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Reveal);
+            // 
+            // character1
+            // 
+            this.character1.AutoSize = true;
+            this.character1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F);
+            this.character1.Location = new System.Drawing.Point(12, 111);
+            this.character1.Name = "character1";
+            this.character1.Size = new System.Drawing.Size(162, 153);
+            this.character1.TabIndex = 1;
+            this.character1.Text = "C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.character1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseForm_KeyUp);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label character1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,48 @@ namespace CTeacher
 {
     public partial class Form1 : Form
     {
+        private List<string> hsk1Unknown = new List<string>();
+        private List<string> hsk1Easy = new List<string>();
+        private List<string> hsk1Hard = new List<string>();
+        private int iterator = 0;
+
         public Form1()
         {
+            this.KeyPreview = true;
             InitializeComponent();
+
+            hsk1Unknown.AddRange(File.ReadAllLines("HSK/hsk1unknown.txt"));
+            hsk1Easy.AddRange(File.ReadAllLines("HSK/hsk1easy.txt"));
+            hsk1Hard.AddRange(File.ReadAllLines("HSK/hsk1hard.txt"));
+
+            Randomise();
+            RevealCharacter();
+        }
+
+        private void Randomise()
+        {
+            Random rand = new Random();
+            hsk1Unknown = hsk1Unknown.OrderBy(_ => rand.Next()).ToList();
+        }
+
+        private void Reveal(object sender, EventArgs e)
+        {
+            RevealCharacter();
+        }
+
+        private void BaseForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                RevealCharacter();
+            }
+            e.Handled = true;
+        }
+
+        private void RevealCharacter()
+        {
+            Console.WriteLine(iterator);
+            character1.Text = hsk1Unknown[iterator++];
         }
     }
 }
